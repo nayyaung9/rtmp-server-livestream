e5280c9a9fb3b98075f7ec5e32561a4036d4a797
nayyaung9 <naygit.developer@gmail.com>
2020-04-12 00:08:40 +0630

    feat: rtmp server init

--- node_modules/node-media-server/api/routes/relay.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const relayController = require('../controllers/relay');
-
-module.exports = (context) => {
-  let router = express.Router();
-  router.post('/pull', relayController.pullStream.bind(context));
-  router.post('/push', relayController.pushStream.bind(context));
-  return router;
-};
